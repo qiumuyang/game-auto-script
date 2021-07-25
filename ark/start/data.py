54c45ed3c6e7b9afc195a4ce2fde0eb0cd2a32a4
QMY <782719906@qq.com>
2021-07-25 22:12:51 +0800

    feat(ark): add game starter

---
+++ ark/start/data.py
@@ -0,0 +1,44 @@
+from enum import IntEnum
+from ..common import ark_intf
+
+intf = ark_intf
+
+GAME_ICON = 'start/icon.png'
+START = 'start/start.png'
+WAKEN = 'start/开始唤醒.png'
+ACCOUNT_MANAGE = 'start/账号管理.png'
+ACCOUNT_LOGIN = 'start/账号登录.png'
+OK = 'ok.png'
+LOGIN = 'start/登录.png'
+ACCOUNT_INPUT = 'start/账号.png'
+PASSWORD_INPUT = 'start/密码.png'
+CONFIRM = 'start/确定.png'
+
+# TODO: add download-new-data check
+class Status(IntEnum):
+    Unknown = -1
+    Not_start = 0
+    Start = 1
+    Wake = 2
+    Wake_fail = 3
+    Login = 4
+    Success = 5
+
+
+Status_pivot = {Status.Not_start: GAME_ICON,
+                Status.Start: START,
+                Status.Wake: [WAKEN, ACCOUNT_MANAGE],
+                Status.Login: ACCOUNT_LOGIN,
+                Status.Wake_fail: 'start/请重新输入登录信息.png',
+                Status.Success: ['系统公告.png', '活动公告.png',
+                                 'main/公告.png', 'main/好友.png']
+                }
+
+
+def get_start_status() -> Status:
+    for status, pivot in Status_pivot.items():
+        if isinstance(pivot, str):
+            pivot = [pivot]
+        if any(intf.img_match(img) for img in pivot):
+            return status
+    return Status.Unknown
---
+++ ark/start/start.py
@@ -0,0 +1,57 @@
+from typing import Tuple
+from .data import *
+import configparser
+import logging
+import time
+import os
+
+logger = logging.getLogger('Start')
+logger.setLevel(logging.DEBUG)
+
+
+def _load_user_account() -> Tuple[str, str]:
+    cdir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(cdir, 'user.config')
+    parser = configparser.ConfigParser()
+    parser.read(path)
+    return tuple(parser.get('user', key)
+                 for key in ['account', 'password'])
+
+
+def _login() -> None:
+    intf.wait_img(LOGIN)
+    account, password = _load_user_account()
+    if intf.img_tap(ACCOUNT_INPUT, 1):
+        # input account
+        intf.text(account)
+        intf.img_tap(CONFIRM)
+        time.sleep(1)
+    if intf.img_tap(PASSWORD_INPUT, 1):
+        # input password
+        intf.text(password)
+        intf.img_tap(CONFIRM)
+        time.sleep(1)
+    intf.img_tap(LOGIN)
+
+
+def start_game() -> None:
+    while 1:
+        status = get_start_status()
+        logger.debug(status)
+        if status == Status.Unknown:
+            pass
+        elif status == Status.Not_start:
+            intf.img_tap(GAME_ICON)
+        elif status == Status.Start:
+            intf.img_tap(START)
+        elif status == Status.Wake:
+            intf.img_tap(WAKEN)
+        elif status == Status.Wake_fail:
+            intf.img_tap(OK)
+        elif status == Status.Login:
+            intf.img_tap(ACCOUNT_LOGIN)
+            _login()
+        elif status == Status.Success:
+            break
+
+        time.sleep(2)
