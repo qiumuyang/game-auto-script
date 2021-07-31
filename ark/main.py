4c36998bf3dea86fb375773bc6c4f60c2285e928
QMY <782719906@qq.com>
2021-07-31 22:36:46 +0800

    feat(ark): update entrance

--- ark/main.py
+++ ark/main.py
@@ -2,11 +2,23 @@ from .battle.battle import execute_single_level
 from .shop.shop import execute_purchase
 from .start.start import start_game
 from .nav.navigate import move_to_last_level, move_to_shop
+from .nav.request import collect_request_reward
+from .nav.friend import collect_friend_credit
 
 
 def simple_routine():
     start_game()
+
+    # 重复前一次作战
     if move_to_last_level():
         execute_single_level()
+
+    # 访问好友基建
+    collect_friend_credit()
+
+    # 信用商店
     move_to_shop()
     execute_purchase()
+
+    # 任务奖励
+    collect_request_reward()
