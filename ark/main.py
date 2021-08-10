c6c0ccb243d441bfa13e9cd6817cf75ce23ab7f7
QMY <782719906@qq.com>
2021-08-10 16:35:41 +0800

    feat(ark): add base entrance

--- ark/main.py
+++ ark/main.py
@@ -1,7 +1,8 @@
+from ark.base.todo import collect_todo
 from .battle.battle import execute_single_level
 from .shop.shop import execute_purchase
 from .start.start import start_game
-from .nav.navigate import move_to_last_level, move_to_shop
+from .nav.navigate import move_to_base, move_to_last_level, move_to_shop
 from .nav.request import collect_request_reward
 from .nav.friend import collect_friend_credit
 
@@ -19,6 +20,10 @@ def simple_routine():
     # 信用商店
     move_to_shop()
     execute_purchase()
+
+    # 基建
+    move_to_base()
+    collect_todo()
 
     # 任务奖励
     collect_request_reward()
