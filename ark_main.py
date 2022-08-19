2c1614cb0942b58592aca36d5a84d7f5258f7a43
QMY <782719906@qq.com>
2022-08-19 21:54:04 +0800

    update entrance

---
+++ ark_main.py
@@ -0,0 +1,5 @@
+from ark.main import simple_routine
+
+
+if __name__ == '__main__':
+    simple_routine()
--- main.py
+++
@@ -1,5 +0,0 @@
-from ark.main import simple_routine
-
-
-if __name__ == '__main__':
-    simple_routine()
